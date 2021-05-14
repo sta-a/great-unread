bb32e0637132b75b190b05fdde0a62440777a707
sta-a <stahla@student.ethz.ch>
2021-05-14 11:20:32 +0200

    overwrite file with updated file from master

--- src/handcrafted_features.py
+++ src/handcrafted_features.py
@@ -206,7 +206,6 @@ class FeatureExtractor(object):
         
         ## load doc2vec chunk embeddings
         doc2vec_chunk_embeddings_path = doc_path.replace("/raw_docs", f"/processed_doc2vec_chunk_embeddings_spc_{sentences_per_chunk}") + ".npz"
-        print(doc2vec_chunk_embeddings_path)
         if os.path.exists(doc2vec_chunk_embeddings_path):
             self.doc2vec_chunk_embeddings = load_list_of_lines(doc2vec_chunk_embeddings_path, "np")
         else:
@@ -244,9 +243,9 @@ class FeatureExtractor(object):
     #     return features
     
     def get_all_features(self):
-        features = {
-            "ratio_of_punctuation_marks": self.get_ratio_of_punctuation_marks,
-            "ratio_of_whitespaces": self.get_ratio_of_whitespaces#,
+        chunk_based_feature_mapping = {
+            # "ratio_of_punctuation_marks": self.get_ratio_of_punctuation_marks,
+            # "ratio_of_whitespaces": self.get_ratio_of_whitespaces,
             # "ratio_of_digits": self.get_ratio_of_digits,
             # "ratio_of_exclamation_marks": self.get_ratio_of_exclamation_marks,
             # "ratio_of_question_marks": self.get_ratio_of_question_marks,
@@ -263,30 +262,48 @@ class FeatureExtractor(object):
             # "bigram_entropy": self.get_word_bigram_entropy,
             # "trigram_entropy": self.get_word_trigram_entropy,
 
-            # ## Features in the list
+            ## Features in the list
             
-            # "flesch_reading_ease_score": self.get_flesch_reading_ease_score, # readability
-            # "unigram_entropy": self.get_word_unigram_entropy, # second order redundancy
-            # "average_paragraph_length": self.get_average_paragraph_length, # structural features
-            # "number_of_indentations": self.get_number_of_indentations, # structural features
-            # 0: self.get_average_sbert_sentence_embedding,
-            # 1: self.get_doc2vec_chunk_embedding,
-            # # skipped greetings since this is not e-mail(structural features)
-            # # skipped types of signature since this is not e-mail(structural features)
-            # # skipped content specific features. added BERT average sentence embedding instead.
-            
-            # #######
+            "flesch_reading_ease_score": self.get_flesch_reading_ease_score, # readability
+            "unigram_entropy": self.get_word_unigram_entropy, # second order redundancy
+            "average_paragraph_length": self.get_average_paragraph_length, # structural features
+            "number_of_indentations": self.get_number_of_indentations, # structural features
+            0: self.get_average_sbert_sentence_embedding,
+            1: self.get_doc2vec_chunk_embedding,
+            # skipped greetings since this is not e-mail(structural features)
+            # skipped types of signature since this is not e-mail(structural features)
+            # skipped content specific features. added BERT average sentence embedding instead.
+
+            #######
         }
-        data = []
+        
+        book_based_feature_mapping = {
+            "doc2vec_intra_textual_variance": self.get_doc2vec_intra_textual_variance,
+            "sbert_intra_textual_variance": self.get_sbert_intra_textual_variance,
+            "doc2vec_stepwise_distance": self.get_doc2vec_stepwise_distance,
+            "sbert_stepwise_distance": self.get_sbert_stepwise_distance
+            # skipped overlap score since it's an intra-book feature
+            # skipped outlier score since it's an intra-book feature
+        }
+        
+        # extract chunk based features
+        chunk_based_features = []
         for chunk in self.chunks:
             current_features = {"book_name": self.doc_path.split("/")[-1][:-4]}
-            for feature_name, feature_function in features.items():
+            for feature_name, feature_function in chunk_based_feature_mapping.items():
                 if isinstance(feature_name, int):
                     current_features.update(feature_function(chunk))
                 else:
                     current_features[feature_name] = feature_function(chunk)
-            data.append(current_features)
-        return data
+            chunk_based_features.append(current_features)
+        
+        # extract book based features
+        book_based_features = {}
+        for feature_name, feature_function in book_based_feature_mapping.items():
+            book_based_features["book_name"] = self.doc_path.split("/")[-1][:-4]
+            book_based_features[feature_name] = feature_function(chunk_based_features)
+        
+        return chunk_based_features, book_based_features
     
     def get_ratio_of_punctuation_marks(self, chunk):
         punctuations = 0
@@ -417,8 +434,43 @@ class FeatureExtractor(object):
         Not implemented for German. If we can find "easy words" in German, then we can implement it ourselves.
         """
         return textstat.gunning_fog(chunk.unidecoded_raw_text)
-
-
-class CorpusFeatureExtractor(object):
-    def __init__(self, all_features):
-        self.all_features = all_features
+    
+    def __get_intra_textual_variance(self, chunks, embedding_type):
+        chunk_embeddings = []
+        for chunk in chunks:
+            if embedding_type == "doc2vec":
+                chunk_embeddings.append(chunk.doc2vec_chunk_embedding)
+            elif embedding_type == "sbert":
+                chunk_embeddings.append(np.array(chunk.sbert_sentence_embeddings).mean(axis=0))
+            else:
+                raise Exception(f"Not a valid embedding type {embedding_type}")
+        average_chunk_embedding = np.array(chunk_embeddings).mean(axis=0)
+        euclidean_distances = [np.linalg.norm(average_chunk_embedding - chunk_embedding) for chunk_embedding in chunk_embeddings]
+        return np.mean(euclidean_distances)
+    
+    def get_doc2vec_intra_textual_variance(self, chunks):
+        return self.__get_intra_textual_variance(chunks, "doc2vec")
+    
+    def get_sbert_intra_textual_variance(self, chunks):
+        return self.__get_intra_textual_variance(chunks, "sbert")
+    
+    def __get_stepwise_distance(self, chunks, embedding_type):
+        euclidean_distances = []
+        for chunk_idx in range(1, len(chunks)):
+            if embedding_type == "doc2vec":
+                current_chunk_embedding = chunks[chunk_idx].doc2vec_chunk_embedding
+                previous_chunk_embedding = chunks[chunk_idx - 1].doc2vec_chunk_embedding
+            elif embedding_type == "sbert":
+                current_chunk_embedding = np.array(chunks[chunk_idx].sbert_sentence_embeddings).mean(axis=0)
+                previous_chunk_embedding = np.array(chunks[chunk_idx - 1].sbert_sentence_embeddings).mean(axis=0)
+            else:
+                raise Exception(f"Not a valid embedding type {embedding_type}")
+            euclidean_distances.append(np.linalg.norm(current_chunk_embedding - previous_chunk_embedding))
+        return np.mean(euclidean_distances)
+        
+    def get_doc2vec_stepwise_distance(self, chunks):
+        return self.__get_stepwise_distance(chunks, "doc2vec")
+    
+    def get_sbert_stepwise_distance(self, chunks):
+        return self.__get_stepwise_distance(chunks, "sbert")
+    
